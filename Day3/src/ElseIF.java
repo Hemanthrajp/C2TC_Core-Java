524a3a00563e346f9c904a6c3faaf00e350e0b64
Hemanthraj P <Hemanthraj P@HEMANTHRAJP>
2022-07-20 13:43:14 +0530

    first commit

---
+++ Day3/src/ElseIF.java
@@ -0,0 +1,17 @@
+
+public class ElseIF {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String lang = "Java";
+		if(lang== "python") {
+			System.out.println("language is python");
+		}else if(lang=="C++") {
+			System.out.println("lang is C++");
+		}else if(lang=="java") {
+			System.out.println("lang is java");
+		}else {
+			System.out.println(lang);
+	}
+}
+}
