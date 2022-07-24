a5178de844fe0a2ac848efdcce1c877c318c7223
Hemanthraj P <Hemanthraj P@HEMANTHRAJP>
2022-07-24 18:06:54 +0530

    first commit

---
+++ Day7/src/Example.java
@@ -0,0 +1,12 @@
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+        int b=10;
+        int mult=a*b;
+        System.out.print(mult);
+	}
+
+}
---
+++ Day7/src/NestedFOREx5.java
@@ -0,0 +1,16 @@
+
+public class NestedFOREx5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 4;
+        int m = 5;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+	}
+
+}
