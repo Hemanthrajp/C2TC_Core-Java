cbefda4ba0cafe934713a63608c8f5de48d9a287
Hemanthraj P <Hemanthraj P@HEMANTHRAJP>
2022-07-24 14:52:37 +0530

    first commit

---
+++ Day4/src/ClassInstantiation.java
@@ -0,0 +1,13 @@
+ class Demo {
+int x;
+}
+ class ClassInstantiation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Demo myObj = new Demo();
+		myObj.x = 40;
+		System.out.println(myObj.x);
+	}
+
+}
---
+++ Day4/src/ClassInstantiation6.java
@@ -0,0 +1,16 @@
+
+public class ClassInstantiation6 {
+	int x, y, z;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClassInstantiation6 myObj = new ClassInstantiation6();
+		myObj.x=1;
+		myObj.y=2;
+		myObj.z=3;
+		System.out.println(myObj.x);
+		System.out.println(myObj.y);
+		System.out.println(myObj.z);
+	}
+
+}
