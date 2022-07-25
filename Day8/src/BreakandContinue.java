18fe6cf11b89a1829ed10af8dbbb2f86e982c4dd
Hemanthraj P <Hemanthraj P@HEMANTHRAJP>
2022-07-25 13:12:48 +0530

    first commit

---
+++ Day8/src/BreakandContinue.java
@@ -0,0 +1,17 @@
+
+public class BreakandContinue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1; i<=10; i++)
+        {
+       	 if(i == 1)
+       	 {
+       		 //break;    
+       		 continue;
+       	 }
+       	 System.out.println(i);
+        }
+	}
+
+}
