c35bae465f272903178a374449e84bd4b10edee5
Hemanthraj P <Hemanthraj P@HEMANTHRAJP>
2022-07-25 13:44:07 +0530

    first commit

---
+++ Day15/src/OneDArrayEnhaFOR.java
@@ -0,0 +1,11 @@
+
+public class OneDArrayEnhaFOR {
+
+	public static void main(String[] args) {
+		int[] arr = {12,4,5,2,5};
+		for(int i:arr) {
+			System.out.print(i);
+		}
+	}
+
+}
---
+++ Day15/src/OneDarray.java
@@ -0,0 +1,11 @@
+
+public class OneDarray {
+
+	public static void main(String[] args) {
+		int[] arr = {12,4,5,2,5};
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+
+}
